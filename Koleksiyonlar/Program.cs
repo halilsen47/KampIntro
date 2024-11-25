958dfdaa5b53ff7a611cb1db7518b16b42db9d96
halilsen <hsen8897@gmail.com>
2024-11-25 19:07:25 +0300

    Collections

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Koleksiyonlar;
+
+class Program
+{
+    static void main(string[] args)
+    {
+        string[] isimler = new string[]
+        {
+            "Engin",
+            "Murat",
+            "Kerem",
+            "Halil"
+        };
+
+        Console.WriteLine(isimler[0]);
+
+
+    }
+}
