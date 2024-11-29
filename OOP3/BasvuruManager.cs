6ec71a51900775ff4a0b45ff822dc795e8c5de39
halilsen <hsen8897@gmail.com>
2024-11-29 15:56:44 +0300

    #

--- OOP3/BasvuruManager.cs
+++ OOP3/BasvuruManager.cs
@@ -8,11 +8,17 @@ namespace OOP3
 {
     internal class BasvuruManager
     {
-        public void BasvuruYap(IKrediManager krediManager)
+
+        //Method injection
+        public void BasvuruYap(IKrediManager krediManager, List<ILoggerService> loggerService)
         {
             //Başvuran bilgilerini değerlendirme
 
             krediManager.Hesapla();
+            foreach (ILoggerService loggerservice in loggerService) 
+            {
+                loggerservice.Log();
+            }
         }
         
         public void KrediOnBilgilendirmesiYap(List<IKrediManager> krediler)
--- OOP3/Program.cs
+++ OOP3/Program.cs
@@ -5,6 +5,9 @@ namespace OOP3;
 
 class Program
 {
+    //SOLİD - O - OPEN CLOSED METHOD eklenen bir kredi veya log servisi akışı bozmaz
+    //(YENİ BİR KREDİ EKLEDİĞİNDE DE YENİ BİR LOG EKLEDİĞİNDE DE SİSTEM SIKINTISIZ ÇALIŞMALI)
+    //ESNAF KREDİSİ VE SMS LOG SONRADAN EKLENDİ 
     static void Main(string[] args)
     {
 
@@ -14,12 +17,25 @@ class Program
 
         IKrediManager konutKrediManager = new KonutKrediManager();
         
+        ILoggerService databaseLoggerService = new DataBaseLoggerService();
+        ILoggerService fileLoggerService = new FileLoggerService();
+
+
+        List<ILoggerService> loggers = new List<ILoggerService>()
+        {
+            new DataBaseLoggerService(),
+            new SmsLoggerService(),
+        };
+
         BasvuruManager basvuruManager = new BasvuruManager();
-        //basvuruManager.BasvuruYap(ihtiyacKrediManager);
-        
+        basvuruManager.BasvuruYap(new EsnafKrediManager(),loggers);
+
+        //SOLİD - O su esnaf kredisi ve sms log servis eklendi ama sistem bozulmadı
+        //basvuruManager.BasvuruYap(new EsnafKrediManager(),new SmsLoggerService());
+
         List<IKrediManager>  krediler = new List<IKrediManager>(){ihtiyacKrediManager,tasıtKrediManager,konutKrediManager};
 
-        basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+        //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
 
     }
 
